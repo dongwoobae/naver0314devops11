3b8dbdecff6175bb5e17fee393369b1f0e246c07
dongwoobae <dw5817@naver.com>
2024-03-18 17:06:16 +0900

    0318 수업

--- javawork/naver0314JavaProject/src/day0318/practice.java
+++ javawork/naver0314JavaProject/src/day0318/practice.java
@@ -6,12 +6,16 @@ public class practice {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int score;
-		String msg;
+		System.out.println("구구단 출력");
 		
-		System.out.println("점수?");
+		for(int i=2;i<=9;i++)
+		{
+			for(int j=1;j<=9;j++)
+			{
+				System.out.printf("%d * %d = %d\n",i,j,i*j);
+			}
+		}
 		
-		score = sc.nextInt();
 		
 		
 			
